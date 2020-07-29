d6df804ba1e4e4e79dfbc9f7b0373c7ab25f4369
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-07-29 22:20:35 +0100

    Created webpack.config.js with manifest plugin

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+const config = {
+  entry: "./public/index.js",
+  output: {
+    path: __dirname + "/public/dist",
+    filename: "bundle.js",
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new WebpackPwaManifest({
+      filename: "manifest.json",
+      inject: false,
+      fingerprints: false,
+      name: "Budget Tracker",
+      short_name: "Budget Tracker",
+      description: "An application for tracking your budget",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      start_url: "/",
+      icons: [
+        {
+          src: path.resolve("public/assets/icons/icon-512x512.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join("assets", "icons"),
+        },
+      ],
+    }),
+  ],
+};
+
+module.exports = config;
